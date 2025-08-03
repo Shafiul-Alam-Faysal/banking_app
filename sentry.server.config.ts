fe9bbf053814d6c0d1263f2ee9bb3ac18e31d044
Faysal <faysal179038@gmail.com>
2025-08-03 23:52:13 +0600

    Implement auth, appwrite, and sentry

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -5,15 +5,14 @@
 import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
-  dsn: "https://e300b41df2ffb6d4592972400a25c0d3@o4506876178464768.ingest.us.sentry.io/4507159179034624",
+  dsn: "https://72666c4aca229da0809a4fae2bb83a96@o4509781247262720.ingest.us.sentry.io/4509781249097728",
 
-  // Adjust this value in production, or use tracesSampler for greater control
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
   tracesSampleRate: 1,
+
+  // Enable logs to be sent to Sentry
+  enableLogs: true,
 
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
   debug: false,
-
-  // uncomment the line below to enable Spotlight (https://spotlightjs.com)
-  // spotlight: process.env.NODE_ENV === 'development',
-  
 });
